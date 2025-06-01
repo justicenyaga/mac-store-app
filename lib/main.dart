9e1d9802fa4fc0ac6942bad4b3313a7e668e3770
Justice Nyaga <justiceevansmwema@gmail.com>
2025-06-01 15:20:31 +0300

    implement main screen's bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mac_store_app/views/screens/authentication_screens/login_screen.dart';
+// import 'package:mac_store_app/views/screens/authentication_screens/login_screen.dart';
+import 'package:mac_store_app/views/screens/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginScreen(),
+      home: const MainScreen(),
     );
   }
 }
--- lib/views/screens/main_screen.dart
+++ lib/views/screens/main_screen.dart
@@ -11,8 +11,45 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Welcome"),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              "assets/icons/home.png",
+              width: 25,
+            ),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              "assets/icons/love.png",
+              width: 25,
+            ),
+            label: "Favorite",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              "assets/icons/mart.png",
+              width: 25,
+            ),
+            label: "Store",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              "assets/icons/cart.png",
+              width: 25,
+            ),
+            label: "Cart",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              "assets/icons/user.png",
+              width: 25,
+            ),
+            label: "Account",
+          ),
+        ],
       ),
     );
   }
