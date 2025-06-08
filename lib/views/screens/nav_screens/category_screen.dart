d90836478d558c7994519c071895b0ab6ff13720
Justice Nyaga <justiceevansmwema@gmail.com>
2025-06-08 22:55:15 +0300

    implement the left side of the categories screen
    - Render categories in a ListView

--- lib/views/screens/nav_screens/category_screen.dart
+++ lib/views/screens/nav_screens/category_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mac_store_app/controllers/category_controller.dart';
+import 'package:mac_store_app/models/category_model.dart';
 import 'package:mac_store_app/views/screens/nav_screens/widgets/header_widget.dart';
 
 class CategoryScreen extends StatefulWidget {
@@ -9,12 +12,63 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  late Future<List<Category>> _futureCategories;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureCategories = CategoryController().loadCategories();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        HeaderWidget(),
-      ],
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(MediaQuery.of(context).size.height * 20),
+        child: const HeaderWidget(),
+      ),
+      body: Row(
+        children: [
+          // Left side - Display categories
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.grey.shade200,
+              child: FutureBuilder(
+                future: _futureCategories,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text("Error: ${snapshot.error}"),
+                    );
+                  } else {
+                    final categories = snapshot.data!;
+                    return ListView.builder(
+                      itemCount: categories.length,
+                      itemBuilder: (context, index) {
+                        final category = categories[index];
+                        return ListTile(
+                          title: Text(
+                            category.name,
+                            style: GoogleFonts.quicksand(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
